c1108912a114e5996e13774dd5de73224071c011
Neelansh Mathur <53081208+neelansh15@users.noreply.github.com>
2022-12-03 15:18:55 +0530

    ft: add auth middleware

---
+++ webapp/middleware/authMiddleware.js
@@ -0,0 +1,37 @@
+import expressAsyncHandler from "express-async-handler"
+import { verifyToken } from "../utils/jwt.js"
+
+const protect = expressAsyncHandler(async (req, res, next) => {
+    let token
+    if (
+        req.headers.authorization &&
+        req.headers.authorization.startsWith("Bearer")
+    ) {
+        token = req.headers.authorization.split(" ")[1]
+        const tokenVerificationResult = verifyToken(token)
+
+        if (tokenVerificationResult.success) {
+            req.user = tokenVerificationResult.result
+            next()
+        } else {
+            console.log(tokenVerificationResult.error)
+            res.status(401)
+            throw new Error("Not authorized, token failed")
+        }
+    }
+    if (!token) {
+        res.status(401)
+        throw new Error("Not authorized, no token")
+    }
+})
+
+const admin = (req, res, next) => {
+    if (req.user && req.user.username && req.user.username.isAdmin) {
+        next()
+    } else {
+        res.status(401)
+        throw new Error("Not authorized as an admin")
+    }
+}
+
+export { protect, admin }
