2fefcb38cb6f66f9a192881047bb63dd86c614e0
Neelansh Mathur <53081208+neelansh15@users.noreply.github.com>
2022-12-03 19:24:47 +0530

    fix: File import for IPFS

--- webapp/utils/ipfs.js
+++ webapp/utils/ipfs.js
@@ -1,10 +1,7 @@
 import { IPFS_TOKEN } from "../env.js"
 // import { Readable } from "stream"
-import { Web3Storage } from 'web3.storage'
+import { Web3Storage, File } from 'web3.storage'
 
-const getAccessToken = () => IPFS_TOKEN
-
-const makeStorageClient = () => new Web3Storage({ token: getAccessToken() })
 
 const upload = async (fileName, fileContent) => {
     try {
@@ -15,8 +12,8 @@ const upload = async (fileName, fileContent) => {
             new File([Buffer.from(fileContent)], fileName)
         ]
 
-        const client = makeStorageClient()
-        const cid = client.put(files)
+        const client = new Web3Storage({ token: IPFS_TOKEN })
+        const cid = await client.put(files)
 
         return { success: true, cid }
     } catch (error) {
