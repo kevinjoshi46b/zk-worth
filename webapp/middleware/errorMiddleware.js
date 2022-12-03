0be47d3ec6cebe518f51960139d36f39f4fe1e0d
Neelansh Mathur <53081208+neelansh15@users.noreply.github.com>
2022-12-03 15:22:40 +0530

    ft: add error middleware

---
+++ webapp/middleware/errorMiddleware.js
@@ -0,0 +1,18 @@
+import { NODE_ENV } from "../env.js"
+
+const notFound = (req, res, next) => {
+    const error = new Error(`Not Found - ${req.originalUrl}`)
+    res.status(404)
+    next(error)
+}
+
+const errorHandler = (err, req, res, next) => {
+    const statusCode = res.statusCode === 200 ? 500 : res.statusCode
+    res.status(statusCode)
+    res.json({
+        message: err.message,
+        stack: NODE_ENV === "production" ? null : err.stack,
+    })
+}
+
+export { notFound, errorHandler }
