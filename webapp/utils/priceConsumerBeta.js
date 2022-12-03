1d6c0f46a2fe14e09857f51121386edb244c46d1
Rajas-B <bondalerajas@gmail.com>
2022-12-04 04:09:06 +0530

    Added get price for covalent

--- webapp/utils/priceConsumerBeta.js
+++ webapp/utils/priceConsumerBeta.js
@@ -6,28 +6,29 @@ import {
 } from "../env.js"
 
 
-const getPrice = (token, network, address) => {
+const getPrice = (chainId, address) => {
 
-    const chainId = '80001'
     const url = new URL(`https://api.covalenthq.com/v1/${chainId}/address/${address}/balances_v2/`);
 
     url.search = new URLSearchParams({
         key: COV_API_KEY
     })
+    let res = {}
     fetch(url)
     .then((resp) => resp.json())
     .then(function(data) {
         const result = data.data;
-        console.log(result)
-        console.log(parseInt(result.items[0].balance))
-        console.log(parseInt(result.items[0].balance)*result.items[0].quote_rate*10**-18)
-        return result
+        result.items.forEach(item => {
+            if (item.contract_ticker_symbol in res){
+                res[item.contract_ticker_symbol][0] += item.quote
+                res[item.contract_ticker_symbol][1] += parseInt(item.balance) * 10**-18
+            }
+           res[item.contract_ticker_symbol] = [item.quote, parseInt(item.balance) * 10**-18] 
+        });
+        return res;
     })
 }
 
-// console.log(getPrice("0xBA47cF08bDFbA09E7732c0e48E12a11Cd1536bcd", "polygonMumbai", "0xc3ef7bba19A079E226B9e0D8d91B95ee0a62dE49"))
-
 export {
-    getPrice
+    getPrice,
 };
-
