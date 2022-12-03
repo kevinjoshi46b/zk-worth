d58f9fc363a3ab9374746faa44e7537906b51108
Neelansh Mathur <53081208+neelansh15@users.noreply.github.com>
2022-12-03 17:59:25 +0530

    ft: utils zkp, use compiled wasm and zkey

---
+++ webapp/utils/zkp.js
@@ -0,0 +1,17 @@
+import { groth16 } from "snarkjs"
+
+const generateProof = async (witness) => {
+    const { proof, publicSignals } = await groth16.fullProve(
+        witness,
+        "./utils/zkp/netWorthThreshold.wasm",
+        "./utils/zkp/netWorthThreshold_final.zkey"
+    )
+    return { proof, publicSignals }
+}
+
+const generateCallData = async ({ proof, publicSignals }) => {
+    const callData = await groth16.exportSolidityCallData(proof, publicSignals)
+    return callData
+}
+
+export { generateProof, generateCallData }
