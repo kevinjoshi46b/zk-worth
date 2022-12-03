f62dfc4961778041213626fe52f05c9fbbd5d968
Neelansh Mathur <53081208+neelansh15@users.noreply.github.com>
2022-12-03 11:52:36 +0530

    ft: add `ADMIN_USERNAMES` in env

--- webapp/env.js
+++ webapp/env.js
@@ -3,6 +3,7 @@ const PORT = process.env.PORT || 2000
 const ALCHEMY_GOERLI = process.env.ALCHEMY_GOERLI || ""
 const ALCHEMY_POLYGONMUMBAI = process.env.ALCHEMY_POLYGONMUMBAI || ""
 const WALLET_PRIVATE_KEY = process.env.WALLET_PRIVATE_KEY || ""
+const ADMIN_USERNAMES = JSON.parse(process.env.ADMIN_USERNAMES) || []
 
 export {
     NODE_ENV,
@@ -10,4 +11,5 @@ export {
     ALCHEMY_GOERLI,
     ALCHEMY_POLYGONMUMBAI,
     WALLET_PRIVATE_KEY,
+    ADMIN_USERNAMES
 }
