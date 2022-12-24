f988729cdcae111e407b6a77b9eb3bc2f6a0d045
Kevin Joshi <kevinjoshi46b@gmail.com>
2022-12-24 10:18:52 +0530

    feat: updated frontend packages and nuked screens

--- webapp/frontend/src/App.js
+++ webapp/frontend/src/App.js
@@ -16,11 +16,8 @@ import AuthScreen from "./screens/AuthScreen"
 import SignupScreen from "./screens/SignupScreen"
 import DashboardScreen from "./screens/DashboardScreen"
 import WalletsScreen from "./screens/WalletsScreen"
-import LinkWalletScreen from "./screens/LinkWalletScreen"
 import RequestsScreen from "./screens/RequestsScreen"
 import PageNotFoundScreen from "./screens/PageNotFoundScreen"
-import SendRequestScreen from "./screens/SendRequestScreen"
-import VerifyProofScreen from "./screens/VerifyProofScreen"
 
 export const ColorModeContext = React.createContext({
     toggleColorMode: () => {},
@@ -110,33 +107,9 @@ function App() {
                                 }
                             />
                             <Route
-                                path="/wallets/linkwallet"
-                                element={
-                                    <LinkWalletScreen
-                                        drawerWidth={drawerWidth}
-                                    />
-                                }
-                            />
-                            <Route
                                 path="/requests"
                                 element={
                                     <RequestsScreen drawerWidth={drawerWidth} />
-                                }
-                            />
-                            <Route
-                                path="/requests/sendrequest"
-                                element={
-                                    <SendRequestScreen
-                                        drawerWidth={drawerWidth}
-                                    />
-                                }
-                            />
-                            <Route
-                                path="/requests/verifyproof"
-                                element={
-                                    <VerifyProofScreen
-                                        drawerWidth={drawerWidth}
-                                    />
                                 }
                             />
                             <Route
